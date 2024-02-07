e7b39eb128a3c26d812e8e7ae4857d3051f69899
Julian Gonzalez <julian_gonzalez@epam.com>
2024-02-07 11:32:58 -0500

    old ngFor implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RecipesListComponent } from './recipes-list/recipes-list.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RecipesListComponent],
+  imports: [RecipesListComponent, RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/recipes-list/recipes-list.component.ts
+++ src/app/recipes-list/recipes-list.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../types';
+import { fakeRecipes } from '../fake-data';
+//import { CommonModule } from '@angular/common'; required to old ngFor implementation, also include it 
+// ino imports: [] bellow
 
 @Component({
   selector: 'app-recipes-list',
@@ -7,6 +11,14 @@ import { Component } from '@angular/core';
   templateUrl: './recipes-list.component.html',
   styleUrl: './recipes-list.component.css'
 })
-export class RecipesListComponent {
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [];
+
+  constructor(){}
+
+  ngOnInit(){
+    this.recipes = fakeRecipes;
+
+  }
 
 }
